14ccad0a3529834c4013f5205e761df13f3a304d
Jesús Oviedo Riquelme <j92riquelme@gmail.com>
2018-06-19 00:06:13 -0400

    commit Jose Florentin

---
+++ SistemaDeReservaDeAulas/ModeloDeNegocio/DetalleRol.vb
@@ -0,0 +1,80 @@
+﻿Imports ModeloDeNegocio.Util
+
+Public Class DetalleRol
+    Private id_rol As Integer
+    Private id_permiso As Integer
+
+    Public Property pId_rol As Integer
+        Get
+            Return id_rol
+        End Get
+        Set(value As Integer)
+            id_rol = value
+        End Set
+    End Property
+
+    Public Property pId_permiso As Integer
+        Get
+            Return id_permiso
+        End Get
+        Set(value As Integer)
+            id_permiso = value
+        End Set
+    End Property
+
+    'metodos
+    Public Sub InsertarDetalleRol()
+        Try
+            gDatos.Ejecutar("SpInsertarDetalleRol", Me.id_rol, Me.id_permiso)
+        Catch ex As Exception
+            Throw ex
+        End Try
+    End Sub
+
+    Public Sub ActualizarDetalleRol()
+        Try
+            gDatos.Ejecutar("SpActualizarDetalleRol", Me.id_rol, Me.id_permiso)
+        Catch ex As Exception
+            Throw ex
+        End Try
+    End Sub
+
+    Public Sub EliminarDetalleRol()
+        Try
+            gDatos.Ejecutar("SpEliminarDetalleRol", Me.id_rol, Me.id_permiso)
+        Catch ex As Exception
+            Throw ex
+        End Try
+    End Sub
+
+    Public Shared Function RecuperarDetalleRol() As DataTable
+        Try
+            Dim dtDetalleRol As New DataTable
+            dtDetalleRol = gDatos.TraerDataTable("SpConsultarDetalleRol", 0)
+            Return dtDetalleRol
+        Catch ex As Exception
+            Throw ex
+        End Try
+    End Function
+
+    Public Function RecuperarDetalleRol(vId_rol As Integer) As DetalleRol
+        Try
+            Dim dtDetalleRol As New DataTable
+            dtDetalleRol = gDatos.TraerDataTable("SpConsultarDetalleRol", vId_rol)
+            If dtDetalleRol.Rows.Count > 0 Then
+                Dim vDetalleRol As New DetalleRol
+                With vDetalleRol
+                    .id_rol = dtDetalleRol.Rows(0).Item("id_rol")
+                    .id_permiso = dtDetalleRol.Rows(0).Item("id_permiso")
+                End With
+                Return vDetalleRol
+            Else
+                Return Nothing
+            End If
+        Catch ex As Exception
+            Throw ex
+        End Try
+    End Function
+
+End Class
+
---
+++ SistemaDeReservaDeAulas/SistemaDeReservaDeAulas/Controllers/RolController.vb
@@ -0,0 +1,62 @@
+﻿Imports System.Web.Mvc
+Imports ModeloDeNegocio
+
+Namespace Controllers
+    Public Class RolController
+        Inherits Controller
+
+        <HttpGet()>
+        Function Index() As ActionResult
+            Dim dtRol As New DataTable
+            dtRol = Rol.RecuperarRol()
+            ViewData("Roles") = dtRol.AsEnumerable
+            Return View()
+        End Function
+
+        <HttpGet()>
+        Function Create() As ActionResult
+            Return View()
+        End Function
+
+        <HttpPost()>
+        Function Create(form As FormCollection) As ActionResult
+            Dim vRol As New Rol
+            With vRol
+                .pNombre_rol = form("txtNombre_rol")
+                .InsertarRol()
+            End With
+            Return RedirectToAction("Index")
+        End Function
+
+        <HttpGet()>
+        Function Edit(id As Integer) As ActionResult
+            Dim vRol As New Rol
+            vRol = vRol.RecuperarRol(id)
+
+            Return View(vRol)
+        End Function
+
+        <HttpPost()>
+        Function Edit(form As FormCollection) As ActionResult
+            Dim vRol As New Rol
+            With vRol
+                .pId_rol = form("txtId_rol")
+                .pNombre_rol = form("txtNombre_rol")
+                .ActualizarRol()
+            End With
+            Return RedirectToAction("Index")
+        End Function
+
+        <HttpGet()>
+        Function Delete(id As Integer) As ActionResult
+            Dim vRol As New Rol
+            With vRol
+                .pId_rol = id
+                .EliminarRol()
+            End With
+            Return RedirectToAction("Index")
+        End Function
+
+
+    End Class
+End Namespace
